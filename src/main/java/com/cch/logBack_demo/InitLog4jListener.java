11e35f7887422f570aa2fc43d314f9def102a973
cch <1136856486@qq.com>
2018-09-18 22:21:45 +0800

    asdfgh

--- src/main/java/com/cch/logBack_demo/InitLog4jListener.java
+++ src/main/java/com/cch/logBack_demo/InitLog4jListener.java
@@ -25,23 +25,25 @@ public class InitLog4jListener implements ServletContextListener{
 
     public void contextInitialized(ServletContextEvent arg0)  { 
     	ServletContext sc = arg0.getServletContext();  
-    	String log4jLocation = sc.getInitParameter("log4j_pro_path"); 
-    	 if (log4jLocation == null) {  
-             System.err.println("*** 没有 log4j-properties-location 初始化的文件, 所以使用 BasicConfigurator初始化");  
-             BasicConfigurator.configure();  
-         } else {  
-             String webAppPath = sc.getRealPath("");  
-             String log4jProp = webAppPath + log4jLocation;  
-             File file = new File(log4jProp);  
-             if (file.exists()) {  
-                 System.out.println("使用: " + log4jProp+"初始化日志设置信息");  
-                 PropertyConfigurator.configure(log4jProp);  
-             } else {  
-                 System.err.println("*** " + log4jProp + " 文件没有找到， 所以使用 BasicConfigurator初始化");  
-                 BasicConfigurator.configure();  
-             }  
-         }  
-    	 System.setProperty("MY_HOME", sc.getRealPath("/")+"logs");
+//    	String log4jLocation = sc.getInitParameter("log4j_pro_path"); 
+//    	 if (log4jLocation == null) {  
+//             System.err.println("*** 没有 log4j-properties-location 初始化的文件, 所以使用 BasicConfigurator初始化");  
+//             BasicConfigurator.configure();  
+//         } else {  
+//             String webAppPath = sc.getRealPath("");  
+//             String log4jProp = webAppPath + log4jLocation;  
+//             File file = new File(log4jProp);  
+//             if (file.exists()) {  
+//                 System.out.println("使用: " + log4jProp+"初始化日志设置信息");  
+//                 PropertyConfigurator.configure(log4jProp);  
+//             } else {  
+//                 System.err.println("*** " + log4jProp + " 文件没有找到， 所以使用 BasicConfigurator初始化");  
+//                 BasicConfigurator.configure();  
+//             }  
+//         }  
+    	 System.out.println("listener===============");
+    	 System.setProperty("MY_HOME", sc.getRealPath("/")+"WEB-INF/backlogs");
     	 System.setProperty("log.root.level", "debug");
+    	 System.out.println("listener===============");
     }
 }
